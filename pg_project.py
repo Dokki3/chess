736b601af4f7a978a179508438859ba7904f1158
Dokki <51916722+Dokki3@users.noreply.github.com>
2022-01-20 23:19:39 +0300

    фигуры

--- pg_project.py
+++ pg_project.py
@@ -11,7 +11,7 @@ all_sprites = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('/Users/roman/PycharmProjects/pythonProject10', name)
+    fullname = os.path.join('data/', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
